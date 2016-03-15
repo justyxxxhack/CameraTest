24cef20c549c79acb8f478929fc25c46b78a73b5
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2016-03-15 19:22:19 +0300

    Initial commit

---
+++ src/com/example/cameratest/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.cameratest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+	private ImageView mImageView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mImageView = (ImageView) findViewById(R.id.imageView1);
+	}
+	public void onClick(View view){
+		if(view.getId()==R.id.button1) {
+			dispatchTakePictureIntent();
+		}
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	        Bundle extras = data.getExtras();
+	        Bitmap imageBitmap = (Bitmap) extras.get("data");
+	        mImageView.setImageBitmap(imageBitmap);
+	    }
+	}
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+
+	private void dispatchTakePictureIntent() {
+	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+	    }
+	}
+}
