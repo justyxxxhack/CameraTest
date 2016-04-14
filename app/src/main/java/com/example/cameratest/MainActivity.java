8e1e5f35cfc312dd893c0f3e1aa2b7f0e91881f9
justyxxxhack <a1@MacBook-Pro-Mihail.local>
2016-04-14 22:40:07 +0300

    last changes

--- app/src/main/java/com/example/cameratest/MainActivity.java
+++ app/src/main/java/com/example/cameratest/MainActivity.java
@@ -5,12 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
-import android.graphics.RectF;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -21,108 +19,115 @@ import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
-	private ImageView mImageView;
+    private ImageView mImageView;
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
-    float x,y,z,e,lx,ly,lz;
+    float x, y, z, lx, ly, lz, angle;
     String string;
-    float angle;
+    String imageFileName;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		mImageView = (ImageView) findViewById(R.id.imageView1);
+    protected void onCreate(Bundle savedInstanceState) {
+        //Action onCreate Activity
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.imageView1);
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-	}
-	public void onClick(View view){
-		if(view.getId()==R.id.button1) {
-			dispatchTakePictureIntent();
-            x=lx;
-            y=ly;
-            z=lz;
-            string = "x=" + Float.toString(x) +"y=" + Float.toString(y) + "z=" + Float.toString(z);
+    }
+
+    public void onClick(View view) {
+        //Action onClick button1
+        if (view.getId() == R.id.button1) {
+            //Takes Picture
+            dispatchTakePictureIntent();
+            //Fix accelerometer data
+            x = lx;
+            y = ly;
+            z = lz;
+            string = "x=" + Float.toString(x) + "y=" + Float.toString(y) + "z=" + Float.toString(z);
             //for ASUS Zenfone
             //angle =(float) ((Math.atan(y/x) * 180) / Math.PI);
             //for Samsung Tablet
-            angle =(float) -((Math.atan(x/y) * 180) / Math.PI);
+            angle = (float) -((Math.atan(x / y) * 180) / Math.PI);
         }
-	}
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-	        Bundle extras = data.getExtras();
-	        Bitmap imageBitmap = rotate((Bitmap) extras.get("data"), (float) angle);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Action after successfully take picture
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = rotate((Bitmap) extras.get("data"), (float) angle);
             createImageFileName();
             saveImage(imageBitmap, imageFileName);
-	        mImageView.setImageBitmap(imageBitmap);
-            TextView tv =(TextView) findViewById(R.id.textView);
+            mImageView.setImageBitmap(imageBitmap);
+            TextView tv = (TextView) findViewById(R.id.textView);
             tv.setText(string);
-	}
-}
+        }
+    }
+
     public static Bitmap rotate(Bitmap source, float angle) {
+        //Method that rotates bitmap
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
-        source = Bitmap.createBitmap(source, 0, 0, source.getWidth(),source.getHeight(), matrix, false);
-//        Matrix m = new Matrix();
-//        RectF inRect = new RectF(0, 0, source.getWidth(), source.getHeight());
-//        RectF outRect = new RectF(source.getWidth()/4, source.getHeight()/4, source.getWidth()/2, source.getHeight()/2);
-//        m.setRectToRect(inRect, outRect, Matrix.ScaleToFit.CENTER);
- //       float[] values = new float[9];
- //       m.getValues(values);
-        //resize bitmap
- ///       source = Bitmap.createScaledBitmap(source, source.getWidth(), source.getHeight(), true);
-        source = Bitmap.createBitmap(source, source.getWidth()/4, source.getHeight()/4, source.getWidth()/3,source.getHeight()/3);
+        source = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, false);
+        source = Bitmap.createBitmap(source, source.getWidth() / 4, source.getHeight() / 4, source.getWidth() / 3, source.getHeight() / 3);
         return source;
     }
+
     protected void onPause() {
         super.onPause();
         senSensorManager.unregisterListener(this);
     }
+
     protected void onResume() {
         super.onResume();
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-	static final int REQUEST_IMAGE_CAPTURE = 1;
-
-	private void dispatchTakePictureIntent() {
-	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    private void dispatchTakePictureIntent() {
+        //Intent to take picture
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-	}
+    }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor mySensor = sensorEvent.sensor;
 
-
+        //Get g values from sensor
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             lx = sensorEvent.values[0];
             ly = sensorEvent.values[1];
             lz = sensorEvent.values[2];
         }
-        TextView tv2 =(TextView) findViewById(R.id.textView2);
+        //Output g values
+        TextView tv2 = (TextView) findViewById(R.id.textView2);
         String string2;
-        string2 = "x=" + Float.toString(lx) +"y=" + Float.toString(ly) + "z=" + Float.toString(lz);
+        string2 = "x=" + Float.toString(lx) + "y=" + Float.toString(ly) + "z=" + Float.toString(lz);
         tv2.setText(string2);
+
     }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
     private void saveImage(Bitmap finalBitmap, String image_name) {
-        //save image file to storage
+        //Save image file to storage
         String root = Environment.getExternalStorageDirectory().toString();
         File myDir = new File(root);
         myDir.mkdirs();
-        String fname = "Image-" + image_name+ ".jpg";
+        String fname = "Image-" + image_name + ".jpg";
         File file = new File(myDir, fname);
         if (file.exists()) file.delete();
         Log.i("LOAD", root + fname);
@@ -135,9 +140,9 @@ public class MainActivity extends Activity implements SensorEventListener {
             e.printStackTrace();
         }
     }
-    String imageFileName;
 
-    private void createImageFileName(){
+
+    private void createImageFileName() {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         imageFileName = "JPEG_" + timeStamp + "_";
